3491a28d0727a9be1c44617714da7449920f5df3
Derek Griffin <griffinderek6@gmail.com>
2022-08-10 12:54:40 -0600

    gues my fav number done

---
+++ FavoriteNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FavoriteNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"guess my favoirte number");
+
+            var r = new Random();
+            var favNumber = r.Next(1,1000);
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            
+
+            if (userInput<favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput>favNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("you got it!");
+            }
+        }
+    }
+}
+
